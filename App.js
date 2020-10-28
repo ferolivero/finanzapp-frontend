d5f2cd8a4bd1cc54c6973d42ef39e5ab50724115
FerOlivero <fernando.olivero@comafi.com.ar>
2020-10-28 14:10:36 -0300

    Se sube la pantalla de login y se condiciona si esta logeado o no el usuario.

--- App.js
+++ App.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-import Home from './pages/home/home.js';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 import Agregar from './pages/agregar/agregar.js';
-
+import Home from './pages/home/home.js';
+import LoginScreen from './pages/Login/Login.js';
 
 const Stack = createStackNavigator();
 
 export default function App() {
 
+  const [logged, setLogged] = useState(false);
+
+  const onLoginSuccess = (loggedUser) => { 
+    console.log('Login Success', loggedUser);
+    setLogged(true);
+  }
+
   return (
-    /**
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <LoginScreen></LoginScreen>
-    </View>
-    */
-    <NavigationContainer>
+    <>
+    {
+      (!logged) ? <>
+          <StatusBar style="auto" />
+          <Text>Por favor para continuar inicia sesion.</Text>
+          <LoginScreen onLoginSuccess={onLoginSuccess}></LoginScreen>
+        </>
+    : 
+      <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
             <Stack.Screen options={{title:"Finanzapp"}} name="Home" component={Home}/>
             <Stack.Screen options={{title:"Finanzapp"}} name="Agregar" component={Agregar}/>
         </Stack.Navigator>
     </NavigationContainer>
+    }
+    </>
   );
 }
