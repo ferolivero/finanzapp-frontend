56edfbb47840df672b91ba567bad7e57820d8f3a
Ignacio Sbaraglia <shogakukan@gmail.com>
2020-10-31 00:52:56 -0300

    date picker

--- pages/agregar/agregar.js
+++ pages/agregar/agregar.js
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Switch, Text } from 'react-native';
+import { StyleSheet, View, Button, Text, TouchableOpacity } from 'react-native';
 import FooterAgregar from './components/footerAgregar';
 import SwitchSelector from "react-native-switch-selector";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 
 export default function Agregar({ navigation }) {
-    const [gasto, setGasto] = useState(false);
-    const toggleSwitch = () => setGasto(previousState => !previousState);
     const [color, setColor] = useState('rgb(255,0,0)');
     const [tipo, setTipo] = useState('g');
-    
-    useEffect(()=>{
+    const [fecha, setFecha] = useState(new Date(Date.now()));
+
+
+    useEffect(() => {
         if (tipo === 'i') {
             setColor('rgb(0,125,0)')
         } else {
             setColor('rgb(255,0,0)')
         }
     }, [tipo])
+
+
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = date => {
+        setFecha(date);
+        hideDatePicker();
+    };
 
 
     return (
@@ -38,6 +55,22 @@ export default function Agregar({ navigation }) {
                         { label: "Ingreso", value: "i" }
                     ]}
                 />
+            
+            <View style={styles.row}><Text>{fecha.toDateString()}</Text>
+                <TouchableOpacity  style={{borderWidth:1}} onPress={showDatePicker}>
+                    <Text>Editar</Text>
+                </TouchableOpacity>
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible}
+                    mode="date"
+                    onConfirm={handleConfirm}
+                    onCancel={hideDatePicker}
+                    isDarkModeEnabled={false}
+                    headerTextIOS='Fecha'
+                    cancelTextIOS='Cancelar'
+                    confirmTextIOS='Seleccionar'
+                />
+            </View>
             </View>
             <FooterAgregar navigation={navigation} />
             <StatusBar style="auto" />
@@ -56,16 +89,13 @@ const styles = StyleSheet.create({
     bigContainer: {
         flex: 1,
         padding: 10,
-        width: 300
+        width: 300,
     },
     row: {
         flexDirection: 'row',
         justifyContent: 'center',
         alignContent: 'center',
         marginTop: 10,
-        height: 40,
-        backgroundColor: 'rgb(0,128,0)',
-        borderRadius: 15
     },
     txt20: {
         textAlign: 'center',
