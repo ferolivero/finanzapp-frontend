9ffaa665841cc071f05888d91798e7c69c473c42
Ignacio Sbaraglia <shogakukan@gmail.com>
2020-11-17 20:19:37 -0300

    loader component

---
+++ global-components/loader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyleSheet, View, ActivityIndicator, Text } from 'react-native'
+
+export default function Loader({  }) {
+  return (
+    <View style={styles.loading}>
+      <Text>Cargando</Text>
+      <ActivityIndicator size="large" color="blue" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    opacity: 0.4,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
