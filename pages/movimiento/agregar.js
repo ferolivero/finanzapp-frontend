66814d6f5d295376eba298d8e930e833dff7170a
FerOlivero <fernando.olivero@comafi.com.ar>
2020-11-17 19:56:31 -0300

    Se arregla el agregar movimiento.

--- pages/movimiento/agregar.js
+++ pages/movimiento/agregar.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import Formulario from './components/formulario';
-
-
+import React from 'react'
+import Formulario from './components/formulario'
 
 export default function Agregar({ navigation }) {
-    const mov = { _id: null, tipo: 'gasto', monto: '', descripcion: '', fecha: new Date(Date.now()), categoria: 'Otros' };
+  const mov = {
+    _id: null,
+    tipo: 'gasto',
+    monto: '',
+    descripcion: '',
+    fecha: Date.now(),
+    categoria: 'Otros',
+  }
 
-    return (
-        <Formulario navigation={navigation} movimiento={mov} />
-    );
+  return <Formulario navigation={navigation} movimiento={mov} />
 }
