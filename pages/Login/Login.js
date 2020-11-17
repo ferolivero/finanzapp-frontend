1df8964f8caf34bc6062b692f3ffadc78c5d97d5
FerOlivero <fernando.olivero@comafi.com.ar>
2020-11-17 18:50:48 -0300

    Se agrega un loading mientras ejecuta el request al backend

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -1,49 +1,18 @@
-import { StatusBar } from 'expo-status-bar'
+import axios from 'axios'
+import * as Google from 'expo-google-app-auth'
+import React, { useState } from 'react'
 import {
+  ActivityIndicator,
+  Dimensions,
   Image,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
-  Dimensions,
 } from 'react-native'
-import axios from 'axios'
-import * as Google from 'expo-google-app-auth'
-import React from 'react'
 import { apiConfig } from './../../config/ApiConfig'
 
 let fullWidth = Dimensions.get('window').width - 40 //full width
-
-async function signInWithGoogleAsync(onLoginSuccess) {
-  console.log('Inicia logueo... ')
-  try {
-    const config = {
-      // Se tiene que configurar un clientID para una App en iOS (pueden usar esta que estara disponible hasta el 31 de octubre)
-      iosClientId: `965440611676-l44ls589vsp17lf9nnok70rgbmcndjhn.apps.googleusercontent.com`,
-      // Se tiene que configurar un clientID para una App en Android (pueden usar esta que estara disponible hasta el 31 de octubre)
-      androidClientId: `965440611676-st0f6b7mpuc25k6afr2ha1eq8k6oabf0.apps.googleusercontent.com`,
-    }
-
-    const result = await Google.logInAsync(config)
-    const { type, idToken } = result
-
-    if (type === 'success') {
-      console.log('Inicia el llamado a la api')
-      const params = {
-        method: 'get',
-        url: `${apiConfig.baseUrl}/api/token`,
-        headers: {
-          idToken: idToken,
-        },
-      }
-
-      const response = await axios(params)
-      onLoginSuccess(response.data.accessToken)
-    }
-  } catch (e) {
-    console.error('Error: ', e)
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -75,26 +44,83 @@ const styles = StyleSheet.create({
     fontSize: 15,
     paddingTop: 60,
   },
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    opacity: 0.4,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
 
 export default function LoginScreen(props) {
+  const [loading, setLoading] = useState(false)
+
+  const signInWithGoogleAsync = async (onLoginSuccess) => {
+    setLoading(true)
+    console.log('Inicia logueo... ')
+    try {
+      const config = {
+        // Se tiene que configurar un clientID para una App en iOS (pueden usar esta que estara disponible hasta el 31 de octubre)
+        iosClientId: `965440611676-l44ls589vsp17lf9nnok70rgbmcndjhn.apps.googleusercontent.com`,
+        // Se tiene que configurar un clientID para una App en Android (pueden usar esta que estara disponible hasta el 31 de octubre)
+        androidClientId: `965440611676-st0f6b7mpuc25k6afr2ha1eq8k6oabf0.apps.googleusercontent.com`,
+      }
+
+      const result = await Google.logInAsync(config)
+      const { type, idToken } = result
+
+      if (type === 'success') {
+        console.log('Inicia el llamado a la api')
+        const params = {
+          method: 'get',
+          url: `${apiConfig.baseUrl}/api/token`,
+          headers: {
+            idToken: idToken,
+          },
+        }
+
+        const response = await axios(params)
+        setLoading(false)
+        onLoginSuccess(response.data.accessToken)
+      }
+    } catch (e) {
+      console.error('Error: ', e)
+    }
+  }
+
   console.log(props.onLoginSuccess)
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>FinanzApp</Text>
-      <Text style={styles.subtitle}>Aplicación de Finanzas Personales</Text>
+    <>
+      {loading ? (
+        <View style={styles.loading}>
+          <Text>Cargando</Text>
+          <ActivityIndicator color="blue" size="large" />
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Text style={styles.title}>FinanzApp</Text>
+          <Text style={styles.subtitle}>Aplicación de Finanzas Personales</Text>
 
-      <Text style={styles.text}>Por favor para continuar inicia sesión.</Text>
-      <TouchableOpacity
-        style={styles.buttonGPlusStyle}
-        activeOpacity={0.5}
-        onPress={() => signInWithGoogleAsync(props.onLoginSuccess)}
-      >
-        <Image
-          source={require('./../../assets/btn_google_signin.png')}
-          style={styles.buttonImageIconStyle}
-        />
-      </TouchableOpacity>
-    </View>
+          <Text style={styles.text}>
+            Por favor para continuar inicia sesión.
+          </Text>
+          <TouchableOpacity
+            style={styles.buttonGPlusStyle}
+            activeOpacity={0.5}
+            onPress={() => signInWithGoogleAsync(props.onLoginSuccess)}
+          >
+            <Image
+              source={require('./../../assets/btn_google_signin.png')}
+              style={styles.buttonImageIconStyle}
+            />
+          </TouchableOpacity>
+        </View>
+      )}
+    </>
   )
 }
