078326c15f2e2d6b294893777ea6dc83fdd7cae3
FerOlivero <fernando.olivero@comafi.com.ar>
2020-11-22 16:34:51 -0300

    Se agrega el label de la fecha para que sea amigable para el usuario. Se elimina el button de edit.

--- global-components/inputModalMesAnio.js
+++ global-components/inputModalMesAnio.js
@@ -1,56 +1,82 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import Modal from './modalMesAnio';
-export default function InputModalMesAnio({ label, mes, setMes, anio, setAnio }) {
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import Modal from './modalMesAnio'
+export default function InputModalMesAnio({
+  label,
+  mes,
+  setMes,
+  anio,
+  setAnio,
+}) {
+  const [modalVisible, setModalVisible] = useState(false)
 
-    const [modalVisible, setModalVisible] = useState(false);
+  const monthNames = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ]
+  const getFormatedLabelDate = (mes, anio) => {
+    return `${monthNames[mes - 1]} - ${anio}`
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text>{label}</Text>
-            <View style={styles.row}>
-                <View style={styles.rowItem80}>
-                    <Text style={styles.inputTxt}>{mes} - {anio}</Text>
-                </View>
-                <TouchableOpacity style={styles.rowItem20} onPress={() => setModalVisible(true)}>
-                    <Text style={styles.inputTxt}>Edit</Text>
-                </TouchableOpacity>
-                <Modal
-                    tipo={label}
-                    mes={mes}
-                    setMes={setMes}
-                    anio={anio}
-                    setAnio={setAnio}
-                    modalVisible={modalVisible}
-                    setModalVisible={setModalVisible} />
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>{label}</Text>
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.rowItem80}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.inputTxt}>{getFormatedLabelDate(mes, anio)}</Text>
+        </TouchableOpacity>
+        <Modal
+          tipo={label}
+          mes={mes}
+          setMes={setMes}
+          anio={anio}
+          setAnio={setAnio}
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        />
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-      },
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-    },
-    rowItem80: {
-        flexDirection: 'column',
-        flex: 4,
-        marginRight: 2
-    },
-    rowItem20: {
-        flexDirection: 'column',
-        flex: 1
-    },
-    inputTxt: {
-        borderWidth: 1,
-        fontSize: 25,
-        paddingLeft: 2
-    }
-});
+  container: {
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  rowItem80: {
+    flexDirection: 'column',
+    flex: 4,
+    marginRight: 2,
+  },
+  rowItem20: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+  inputTxt: {
+    textAlign: 'center',
+    borderWidth: 1,
+    fontSize: 25,
+    paddingLeft: 2,
+    marginBottom: 20,
+  },
+})
