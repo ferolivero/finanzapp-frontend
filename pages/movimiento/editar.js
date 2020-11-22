39b689c18b934da84e9df54d87e0525cf3a40dc9
Ignacio Sbaraglia <shogakukan@gmail.com>
2020-11-21 23:34:37 -0300

    se arregló?

--- pages/movimiento/editar.js
+++ pages/movimiento/editar.js
@@ -3,14 +3,28 @@ import { StyleSheet } from 'react-native'
 import getApiClient from '../../api/ApiClient'
 import Loader from './../../global-components/loader'
 import Formulario from './components/formulario'
+import { useFocusEffect } from '@react-navigation/native'
+
 
 export default function Editar({ navigation, route }) {
   const [loading, setLoading] = useState(true)
   const [movimiento, setMovimiento] = useState()
 
   useEffect(() => {
-    buscarMovimiento(route.params.id)
-  }, [route.params.id])
+    setLoading(false)
+  }, [movimiento])
+
+  useEffect(() => {
+    if (loading){
+      buscarMovimiento(route.params.id)
+    }
+  }, [loading])
+
+  useFocusEffect(React.useCallback(() => {
+    console.debug("screen takes focus", route.params.id);
+    setLoading(true)
+    return () => console.debug("screen loses focus");
+  }, []));
 
   const buscarMovimiento = async (id) => {
     const api = await getApiClient()
